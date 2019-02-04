a4a3cd7d3f2bcae6d550f6da81f1eb7cdea362bc
Maximo Bautista <max.bautista97@gmail.com>
2019-02-04 17:28:39 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,8 @@
 class Person
+  
+    attr_accessor :name, :user_name, :age, :location, :bio
+
+
   def initialize(properties)
      properties.each {|key, value| self.send(("#{key}="), value)}
   end
